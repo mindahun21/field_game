8ce06b381add7bc0a2c5e216a9f66747f384d047
mindahun21 <mndahun0@gmail.com>
2024-04-02 13:26:02 +0300

    connectiong databases

--- quiz/models.py
+++ quiz/models.py
@@ -1,5 +1,13 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import JSON, DateTime, ForeignKey, String, Text, func
+from sqlalchemy import String, Text
+
+from enum import Enum as BaseEnum
+
+class Role(BaseEnum):
+    NONE =0
+    USER =1
+    ADMIN =2
+
 
 class Base(DeclarativeBase):
     pass
@@ -7,4 +15,6 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = 'users'
     id:Mapped[int]=mapped_column(primary_key=True)
-    username:Mapped[String]=mapped_column(Text)
+    username:Mapped[str]=mapped_column(Text)
+    chat_id:Mapped[str]=mapped_column(String(255),unique=True,nullable=True)
+    role:Mapped[str]=mapped_column(Text,default=Role.USER)
